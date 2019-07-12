bb937e8cd103c075a998aae1aa38979d0e38e59f
poornimaSapkal <poornimasapkal74@gmail.com>
2019-07-13 00:16:46 +0530

    created landing page and installed packages

---
+++ Destinations/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+app.set("view engine", "ejs");
+
+
+var destinations = [
+    {name: 'Santorini', image: 'https://images.unsplash.com/photo-1539288541332-0efaa4749f34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80'},
+    {name: 'Bali', image: 'https://images.unsplash.com/photo-1557093793-e196ae071479?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'},
+    {name: 'Bora Bora', image: 'https://images.unsplash.com/photo-1500930287596-c1ecaa373bb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'},
+    {name: 'Nice', image: 'https://images.unsplash.com/photo-1551799142-93484f2d0284?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1566&q=80'}
+];
+//===SETUP THE ROUTES
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/destinations", function(req, res){
+    res.render("destinations", {destinations : destinations});
+});
+
+
+//===SETUP THE SERVER 
+app.listen("3000", function(){
+    console.log("The Server is up and running!");
+});
