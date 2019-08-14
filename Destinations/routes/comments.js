e89dd2e8f6ad8093b72c707bee5e558b351e505e
poornimaSapkal <poornimasapkal74@gmail.com>
2019-08-14 12:22:37 -0400

    added destroy route for comments

--- Destinations/routes/comments.js
+++ Destinations/routes/comments.js
@@ -66,6 +66,17 @@ router.put("/:comment_id", function(req, res){
     })
 });
 
+//COMMENTS DESTROY ROUTE 
+router.delete("/:comment_id", function(req, res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/destinations/"+ req.params.id);
+        };
+    })
+});
+
 //middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
